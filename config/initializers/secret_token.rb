af6cd0434b2392929703aec4cefa9ae817342367
Elias Baixas <elias@eresmas.net>
2012-08-16 14:27:22 +0200

    Empty Rails 3.2.7 project
    just a $(rails new monster-fleet)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonsterFleet::Application.config.secret_token = '23a5dec46d9a230d61ad4b16e074f8919eb7a19e49db53cbd8ca111cfb05b754185a38866902b004df20f9a3aae3d01eea08a04ea3c0026dc23ea2026aebbc9f'
